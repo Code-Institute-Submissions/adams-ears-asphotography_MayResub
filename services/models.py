2da855272015204cde4ba648e9f6ccc9943b59f1
adams-ears <adamsears146@gmail.com>
2021-02-27 00:22:57 +0000

    added services fixtures and admin

--- services/models.py
+++ services/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 
-class ServicesProvided(models.Model):
-    service_name = models.CharField(max_length=200)
+class Services(models.Model):
+    name = models.CharField(max_length=200)
     image = models.ImageField(null=True, blank=True)
     description = models.TextField()
 
     def __str__(self):
         return self.name
+
--- services/views.py
+++ services/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Services
 
 
 def services(request):
+    services = Services.objects.all()
 
-    return render(request, 'services/services.html')
+    context = {
+        'services': services,
+    }
+
+    return render(request, 'services/services.html', context)
+
