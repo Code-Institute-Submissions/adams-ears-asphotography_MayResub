df8a96a04b3b62675a01241dbd6e68dac48dc945
adams-ears <adamsears146@gmail.com>
2021-02-27 20:43:44 +0000

    Removed secret key and set debug

--- asphotography/settings.py
+++ asphotography/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v-69v)t83zjkgs0cn)*pbe=#uyx8()7!w9swdky*^c-k#=#9cy'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['asphotography.herokuapp.com', 'localhost']
 
