b2f3e2b53a546e2f90c484c9e55a576a50a1b504
adams-ears <adamsears146@gmail.com>
2021-02-27 12:59:16 +0000

    update services template add basica gallery

--- services/models.py
+++ services/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class Services(models.Model):
+    class Meta:
+        verbose_name_plural = 'Services'
 
     name = models.CharField(max_length=200)
     image = models.ImageField(null=True, blank=True)
