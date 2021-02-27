dd03f71f6cbd1e6011d200809aa2ef698972f885
adams-ears <adamsears146@gmail.com>
2021-02-27 19:00:56 +0000

    heroku deployment phase 2 disable collectstatic

--- asphotography/settings.py
+++ asphotography/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'v-69v)t83zjkgs0cn)*pbe=#uyx8()7!w9swdky*^c-k#=#9cy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['asphotography.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -127,12 +127,18 @@ WSGI_APPLICATION = 'asphotography.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
